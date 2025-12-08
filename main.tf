8588b2a73f372415741f28c4a8badc77caa56a0f
Sampath513 <sampathreddykarra513@gmail.com>
2025-12-08 03:42:36 +0000

    ec2 added

--- main.tf
+++ main.tf
@@ -14,3 +14,14 @@ resource "aws_dynamodb_table" "xyz_dynamodb_table" {
         type = "S"
     }
 }
+resource "aws_instance" "my_instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+   // count = 2
+   // for_each = [ "value" ]
+   //subnet_id = "subnet-0bb1c79de3EXAMPLE"
+   //vpc_security_group_ids = [ "sg-085912345678492fb" ]
+    tags = {
+        Name = "MyInstance"
+    }
+}
