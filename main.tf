197a0284a4489a568f1f9de4fb3d30d562f62acc
Sampath513 <sampathreddykarra513@gmail.com>
2025-12-08 03:37:53 +0000

    resource added for dynamo db

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_s3_bucket" "xyz_bucket" {
         enabled = true
     }
 }
+resource "aws_dynamodb_table" "xyz_dynamodb_table" {
+    name = "xyz-project-dynamodb-table"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
