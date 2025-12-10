6817092bb4f9d6f6b1e80fbfcdfd6a8f3513ace4
Sampath513 <sampathreddykarra513@gmail.com>
2025-12-10 10:06:13 +0000

    file saved

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "xyz-project-bucket-12-23"
-        key = "xyz-project/terraform.tfstate"
+        key = "xyz-project/dev/terraform.tfstate"
         dynamodb_table = "xyz-project-dynamodb-table"
         region = "us-east-1"
     }
